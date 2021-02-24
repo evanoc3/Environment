ea564c1e9769b90a0f3980297548b98256dac9b0
Evan O'Connor <evanoc3@gmail.com>
2021-02-24 01:13:38 +0000

    Replaced individual ZSH custom folder files with .zshrc

--- data/zsh/custom/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias p="cd \"$HOME/dev/\""
-alias mkvenv="python3 -m venv .venv"
-alias venv="source ./.venv/bin/activate"
--- data/zsh/custom/paths.zsh
+++
@@ -1,9 +0,0 @@
-potential_paths=(
-	"$HOME/bin"
-)
-
-for potential_path in $potential_paths; do
-	if [[ -e $potential_path ]]; then
-		export PATH="$PATH:$potential_path"
-	fi
-done
